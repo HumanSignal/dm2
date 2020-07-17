42ccf805cc5dfa8bc6425db59969c161b4e62e5b
hlomzik <hlomzik@gmail.com>
2020-07-18 00:34:43 +0300

    Switch to webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './DM';
 
 ReactDOM.render(<App />, document.getElementById('app'));
