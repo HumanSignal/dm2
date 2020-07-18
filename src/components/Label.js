9c5465b32ceedd54f3fddf78c552fa6aa72b58be
hlomzik <hlomzik@gmail.com>
2020-07-18 23:11:24 +0300

    Add LS to Label All screen

--- src/components/Label.js
+++ src/components/Label.js
@@ -6,21 +6,43 @@ import { Button } from "antd";
 import Table from "./Table";
 
 import data from '../data/tasks.json';
+import config from '../data/config';
+
+const user = { pk: 1, firstName: "James", lastName: "Dean" };
+const interfaces = [
+  "panel",
+  "update",
+  "controls",
+  "side-column",
+  "completions:menu",
+  "completions:add-new",
+  "completions:delete",
+  "predictions:menu",
+];
 
 const DmLabel = inject('store')(observer(({ store }) => {
     const item = store.viewsStore.labelingView;
     const columns = item.fieldsAsColumns;
+
+    const runLS = React.useCallback(task => {
+      if (!window.LabelStudio) return setTimeout(() => runLS(task), 100);
+      new window.LabelStudio('label-studio', { config, interfaces, user, task });
+    }, []);
+
+    React.useEffect(() => runLS(data[0]), [runLS]);
     
     return (
         <div>
+          <link href="https://unpkg.com/label-studio@0.7.3/build/static/css/main.09b8161e.css" rel="stylesheet" />
+
           <Button onClick={() => store.setMode('dm') }>
             Back to Table
           </Button>
 
           <div style={{ display: "flex" }}>
-            <div style={{ width: "200px" }}><Table columns={columns} data={data} item={item} /></div>
+            <div style={{ flex: "200px 0 0", marginRight: "1em" }}><Table columns={columns} data={data} item={item} /></div>
             <div>
-              Label Studio here...
+              <div id="label-studio"></div>
             </div>
           </div>
           
