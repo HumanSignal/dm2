1fd9b1d137d8f606e18bdb7c7b3b0be03c41dea0
Nikita Skryabin <nr@fenelon.ru>
2020-11-09 06:24:50 +0300

    Display lead time

--- src/stores/Annotations/annotation.js
+++ src/stores/Annotations/annotation.js
@@ -5,6 +5,7 @@ export const Annotation = types.model("Annotation", {
   id: types.identifierNumber,
   result: types.optional(types.array(CustomJSON), []),
   task_id: types.integer,
+  lead_time: types.optional(types.maybeNull(types.number), null),
   created_at: types.optional(types.maybeNull(StringOrNumber), null),
   updated_at: types.optional(types.maybeNull(StringOrNumber), null),
   source: types.optional(types.string, ""),
