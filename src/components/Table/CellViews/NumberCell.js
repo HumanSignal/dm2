786ea27932c03be2b7a33b2cca0c88cc93389901
Nikita Skryabin <nr@fenelon.ru>
2021-01-16 16:18:21 +0300

    Properly display empty value for NumberCell

--- src/components/Table/CellViews/NumberCell.js
+++ src/components/Table/CellViews/NumberCell.js
@@ -1,3 +1,6 @@
-export const NumberCell = (column) => Number(column.value);
+import { isDefined } from "../../../utils/utils";
+
+export const NumberCell = (column) =>
+  isDefined(column.value) ? Number(column.value) : "";
 
 NumberCell.userSelectable = false;
