d410ac652c5bf9ceda3898403347e131a248d4fd
Nikita Skryabin <nr@fenelon.ru>
2021-02-26 14:07:04 +0300

    Setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,22 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 const Dotenv = require("dotenv-webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+const BUILD = {
+  NO_MINIMIZE: isDevelopment || !!process.env.BUILD_NO_MINIMIZATION,
+  NO_CHUNKS: isDevelopment || !!process.env.BUILD_NO_CHUNKS,
+  NO_HASH: isDevelopment || process.env.BUILD_NO_HASH,
+  MODULE: !isDevelopment && !!process.env.BUILD_MODULE,
+};
+
+const dirPrefix = {
+  js: isDevelopment ? "" : "static/js/",
+  css: isDevelopment ? "" : "static/css/",
+};
 
 const LOCAL_ENV = {
   NODE_ENV: process.env.NODE_ENV,
@@ -11,6 +27,77 @@ const LOCAL_ENV = {
   SC_DISABLE_SPEEDY: false,
   GATEWAY_API: false,
   HTX_ACCESS_TOKEN: "---",
+};
+
+const babelOptimizeOptions = () => {
+  return BUILD.NO_MINIMIZE
+    ? {
+        compact: false,
+        cacheCompression: false,
+      }
+    : {
+        compact: true,
+        cacheCompression: true,
+      };
+};
+
+const optimizer = () => {
+  const result = {};
+
+  result.minimize = BUILD.NO_MINIMIZE ? false : true;
+
+  result.minimizer = BUILD.NO_MINIMIZE
+    ? undefined
+    : [new TerserPlugin(), new CssMinimizerPlugin()];
+
+  result.runtimeChunk = BUILD.NO_CHUNKS ? false : true;
+  result.splitChunks = {
+    cacheGroups: {
+      default: BUILD.NO_CHUNKS ? false : true,
+    },
+  };
+
+  return result;
+};
+
+const output = () => {
+  const result = {
+    filename: "[name]-[contenthash].js",
+    chunkFilename: "[name]-[contenthash]-[id].chunk.js",
+  };
+
+  if (BUILD.NO_HASH) {
+    result.filename = "[name].js";
+    result.chunkFilename = "[name].chunk.js";
+  }
+
+  if (BUILD.MODULE) {
+    result.library = "DataManager";
+    result.libraryExport = "default";
+    result.libraryTarget = "commonjs";
+  }
+
+  result.filename = dirPrefix.js + result.filename;
+  result.chunkFilename = dirPrefix.js + result.chunkFilename;
+
+  return result;
+};
+
+const cssOutput = () => {
+  const result = {
+    filename: "[name]-[contenthash].css",
+    chunkFilename: "[name]-[contenthash]-[id].chunk.css",
+  };
+
+  if (BUILD.NO_HASH) {
+    result.filename = "[name].css";
+    result.chunkFilename = "[name].[contenthash:8].chunk.css";
+  }
+
+  result.filename = dirPrefix.css + result.filename;
+  result.chunkFilename = dirPrefix.css + result.chunkFilename;
+
+  return result;
 };
 
 const babelLoader = {
@@ -33,6 +120,7 @@ const babelLoader = {
       "@babel/plugin-proposal-optional-chaining",
       "@babel/plugin-proposal-nullish-coalescing-operator",
     ],
+    ...babelOptimizeOptions(),
   },
 };
 
@@ -84,16 +172,22 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "main.js",
+    ...output(),
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new Dotenv(),
-    new MiniCssExtractPlugin(),
-    new HtmlWebPackPlugin({ filename: "public/index.html" }),
+    new MiniCssExtractPlugin({
+      ...cssOutput(),
+    }),
+    new HtmlWebPackPlugin({
+      filename: "public/index.html",
+    }),
     new webpack.EnvironmentPlugin(LOCAL_ENV),
   ],
+  optimization: optimizer(),
   module: {
     rules: [
       {
@@ -121,6 +215,18 @@ module.exports = {
           },
           {
             use: cssLoader(),
+          },
+        ],
+      },
+      {
+        test: /\.(webm|mov)$/i,
+        use: [
+          {
+            loader: require.resolve("url-loader"),
+            options: {
+              limit: 200000,
+              encoding: "base64",
+            },
           },
         ],
       },
