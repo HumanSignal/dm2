d58a583bcef02b410db01d57d0515689b79259d9
Nikita Skryabin <nr@fenelon.ru>
2021-05-04 02:54:29 +0300

    Expose user cell click event

--- src/components/Table/CellViews/Annotators/Annotators.js
+++ src/components/Table/CellViews/Annotators/Annotators.js
@@ -1,18 +1,25 @@
+import { useSDK } from "../../../../providers/SDKProvider";
 import { Block, Elem } from "../../../../utils/bem";
 import { Tooltip } from "../../../Common/Tooltip/Tooltip";
 import { Userpic } from "../../../Common/Userpic/Userpic";
 import "./Annotators.styl";
 
-export const Annotators = ({value}) => {
+export const Annotators = (cell) => {
+  const {value, column} = cell;
+  const sdk = useSDK();
   const userList = Array.from(value);
-  const renderable = userList.slice(0, 3);
+  const renderable = userList.slice(0, 10);
   const extra = userList.length - renderable.length;
 
   return (
     <Block name="annotators">
       {renderable.map(user => {
         return (
-          <Elem key={`user-${user.id}`} name="item">
+          <Elem key={`user-${user.id}`} name="item" onClick={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            sdk.invoke("userCellClick", [e, column.alias, user]);
+          }}>
             <Tooltip title={user.fullName || user.email}>
               <Userpic user={user}/>
             </Tooltip>
