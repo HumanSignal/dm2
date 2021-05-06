898a04bf34f5785b34ceb46d0373c0fcd1e1d489
hlomzik <hlomzik@gmail.com>
2021-05-06 11:45:13 +0300

    Add TS to SDK; fix params/types issues
    id->pk issue, creation date

---
+++ src/sdk/lsf-utils.ts
@@ -0,0 +1,76 @@
+import { APITask, APIAnnotation, APIPrediction, LSFAnnotation, LSFTaskData } from "../types/Task";
+
+/**
+ * Converts the task from the server format to the
+ * format supported by the LS frontend
+ * @param {import("../stores/Tasks").TaskModel} task
+ * @private
+ */
+export const taskToLSFormat = (task: APITask): LSFTaskData | void => {
+  if (!task) return;
+
+  console.log("WOW TASK", task.created_at, task);
+
+  const result: LSFTaskData = {
+    ...task,
+    annotations: [],
+    predictions: [],
+    createdAt: task.created_at,
+    // isLabeled: task.is_labeled, // @todo why?
+  };
+
+  if (task.annotations) {
+    result.annotations = task.annotations.map(annotationToLSF);
+  }
+
+  if (task.predictions) {
+    result.predictions = task.predictions.map(predictionToLSF);
+  }
+
+  return result;
+};
+
+export const annotationToLSF = (annotation: APIAnnotation) => {
+  return {
+    ...annotation,
+    id: undefined,
+    pk: String(annotation.id),
+    createdAgo: annotation.created_ago,
+    createdBy: annotation.created_username,
+    createdDate: annotation.created_at,
+    leadTime: annotation.lead_time ?? 0,
+    skipped: annotation.was_cancelled ?? false,
+  };
+};
+
+export const predictionToLSF = (prediction: APIPrediction) => {
+  console.log("WOW PREDICTION", prediction);
+  return {
+    ...prediction,
+    id: undefined,
+    pk: String(prediction.id),
+    createdAgo: prediction.created_ago,
+    createdBy: prediction.model_version?.trim() ?? "",
+    createdDate: prediction.created_at,
+  };
+};
+
+export const annotationToServer = (annotation: LSFAnnotation): APIAnnotation => {
+  return {
+    ...annotation,
+    id: Number(annotation.pk),
+    created_ago: annotation.createdAgo,
+    created_username: annotation.createdBy,
+    created_at: new Date().toISOString(),
+    lead_time: annotation.leadTime,
+  };
+};
+
+export const getAnnotationSnapshot = (c: LSFAnnotation) => ({
+  id: c.id,
+  pk: c.pk,
+  result: c.serializeAnnotation(),
+  leadTime: c.leadTime,
+  userGenerate: !!c.userGenerate,
+  sentUserGenerate: !!c.sentUserGenerate,
+});
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,10 @@
 /**
+ * @todo use [].every()
  * Returns true if all checks return true
  * @param {boolean[]} boolArray
  * @param {(any) => boolean} check
  */
-export const all = (boolArray, check) => {
+export const all = <T>(boolArray: T[], check: (item: T) => boolean) => {
   return boolArray.reduce((res, value) => {
     return res && !!check(value);
   }, true);
@@ -14,18 +15,18 @@ export const all = (boolArray, check) => {
  * @param {boolean[]} boolArray
  * @param {(any) => boolean} check
  */
-export const any = (boolArray, check) => {
+export const any = <T>(boolArray: T[], check: (item: T) => boolean) => {
   return boolArray.find((value) => !!check(value)) || false;
 };
 
-export const randomDate = (start, end) => {
+export const randomDate = (start: Date, end: Date) => {
   return new Date(
     start.getTime() + Math.random() * (end.getTime() - start.getTime())
   );
 };
 
-export const groupBy = (list, group) => {
-  return list.reduce((res, item) => {
+export const groupBy = <T>(list: T[], group: (item: T) => string) => {
+  return list.reduce<Record<string, T[]>>((res, item) => {
     const property = group(item);
 
     if (res[property]) {
@@ -38,14 +39,14 @@ export const groupBy = (list, group) => {
   }, {});
 };
 
-export const unique = (list) => {
-  return Array.from(new Set(list));
+export const unique = <T>(list: T[]): T[] => {
+  return Array.from(new Set<T>(list));
 };
 
-export const cleanArray = (array) => {
+export const cleanArray = <T>(array: T[]): T[] => {
   return array.filter((el) => !!el);
 };
 
-export const isDefined = (value) => {
+export const isDefined = (value: any) => {
   return value !== null && value !== undefined;
 };
