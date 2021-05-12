35377c099b28b9786d875fa8d7d187eb8575ee9b
Nikita Skryabin <nr@fenelon.ru>
2021-05-12 05:18:52 +0300

    Allow passing custom actions on init

--- src/sdk/app-create.js
+++ src/sdk/app-create.js
@@ -46,6 +46,7 @@ export const createApp = async (rootNode, datamanager) => {
     showPreviews: datamanager.showPreviews,
     interfaces: Object.fromEntries(datamanager.interfaces),
     toolbar: datamanager.toolbar,
+    availableActions: Array.from(datamanager.actions.values()).map(({action}) => action)
   });
 
   appStore._sdk = datamanager;
