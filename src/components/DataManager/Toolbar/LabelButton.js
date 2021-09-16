59e3398544fed98664469b0dfc12afea81f97ea3
hlomzik <hlomzik@gmail.com>
2021-09-16 19:41:50 +0300

    [ext] Label All/Label as displayed dropdown

--- src/components/DataManager/Toolbar/LabelButton.js
+++ src/components/DataManager/Toolbar/LabelButton.js
@@ -1,6 +1,13 @@
 import { inject } from "mobx-react";
 import { Button } from "../../Common/Button/Button";
 import { Interface } from "../../Common/Interface";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const Arrow = ({ rotate }) => (
+  <svg fill="currentColor" strokeWidth="0" viewBox="0 0 320 512" height="16" width="16" xmlns="http://www.w3.org/2000/svg" style={{ transform: rotate ? "rotate(180deg)" : undefined }}>
+    <path d="M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"/>
+  </svg>
+);
 
 const injector = inject(({ store }) => {
   const { dataStore, currentView } = store;
@@ -16,20 +23,97 @@ const injector = inject(({ store }) => {
   };
 });
 
-export const LabelButton = injector(({ store, allSelected, selectedCount, canLabel, size, target }) => {
-  const all = selectedCount === 0 || allSelected;
+export const LabelButton = injector(({ store, canLabel, size, target }) => {
+  // const all = selectedCount === 0 || allSelected;
+  const disabled = target === "annotations";
+  const triggerRef = useRef();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => setIsOpen(isOpen => !isOpen), []);
+
+  const handleClickOutside = useCallback(e => {
+    const el = triggerRef.current;
+
+    if (el && !el.contains(e.target)) {
+      setIsOpen(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, { capture: true });
+    };
+  }, []);
+
+  const onLabelAll = () => {
+    localStorage.setItem("dm:labelstream:mode", "all");
+    store.startLabelStream();
+  };
+
+  const onLabelVisible = () => {
+    localStorage.setItem("dm:labelstream:mode", "filtered");
+    store.startLabelStream();
+  };
+
+  const triggerStyle = {
+    width: 20,
+    padding: 0,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: isOpen ? 0 : undefined,
+    boxShadow: "none",
+  };
+  const primaryStyle = {
+    width: 160,
+    padding: 0,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    borderBottomLeftRadius: isOpen ? 0 : undefined,
+  };
+  const secondStyle = {
+    width: 180,
+    padding: 0,
+    display: isOpen ? "flex" : "none",
+    position: "absolute",
+    zIndex: 10,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+  };
 
   return canLabel ? (
     <Interface name="labelButton">
-      <Button
-        look="primary"
-        size={size}
-        style={{ width: 160, padding: 0 }}
-        disabled={target === "annotations"}
-        onClick={() => store.startLabelStream()}
-      >
-        Label {all ? "All" : selectedCount} Task{all || selectedCount > 1 ? "s" : ''}
-      </Button>
+      <div>
+        <div style={{ display: "flex" }}>
+          <Button
+            size={size}
+            disabled={disabled}
+            mod={{ size: "medium", look: "primary", disabled }}
+            style={primaryStyle}
+            onClick={onLabelAll}
+          >
+            Label All
+          </Button>
+          <Button
+            ref={triggerRef}
+            size={size}
+            mod={{ size: "medium", look: "primary", disabled }}
+            style={triggerStyle}
+            onClick={toggleOpen}
+          >
+            <Arrow rotate={isOpen} />
+          </Button>
+        </div>
+        <Button
+          size={size}
+          style={secondStyle}
+          mod={{ size: "medium", disabled }}
+          onClick={onLabelVisible}
+        >
+          Label tasks as displayed
+        </Button>
+      </div>
     </Interface>
   ) : null;
 });
