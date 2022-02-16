d77e0b1b08c215d5fbc1cde49281ad07f859407a
nicholasrq <nr@fenelon.ru>
2022-02-16 19:26:22 +0300

    Use proper FF notation

--- src/utils/feature-flags.js
+++ src/utils/feature-flags.js
@@ -1,5 +1,5 @@
 // Add an interactivity flag to the results to make some predictions' results be able to be automatically added to newly created annotations.
-export const FF_DEV_1621 = "ff_front_dev_1621_interactive_mode_15022022";
+export const FF_DEV_1621 = "ff_front_dev_1621_interactive_mode_150222_short";
 
 function getFeatureFlags() {
   return window.APP_SETTINGS?.feature_flags || {};
