100db8ae51a84caaef338de513db6bdb456e07fe
Nick Skriabin <nr@fenelon.ru>
2022-11-30 17:44:02 +0000

    Introduce granular control over tabs' actions

--- src/components/Common/Tabs/Tabs.js
+++ src/components/Common/Tabs/Tabs.js
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { FaEllipsisV } from "react-icons/fa";
 import { BemWithSpecifiContext } from "../../../utils/bem";
 import { Button } from "../Button/Button";
@@ -18,6 +18,7 @@ export const Tabs = ({
   onChange,
   onAdd,
   tabBarExtraContent,
+  allowedActions,
   addIcon,
 }) => {
   const [selectedTab, setSelectedTab] = useState(activeTab);
@@ -31,23 +32,30 @@ export const Tabs = ({
     if (selectedTab !== activeTab) setSelectedTab(activeTab);
   }, [selectedTab, activeTab]);
 
-  return (
-    <TabsContext.Provider value={{
+  const contextValue = useMemo(() => {
+    return {
       switchTab,
       selectedTab,
+      allowedActions,
       lastTab: children.length === 1,
-    }}>
+    };
+  }, [switchTab, selectedTab, allowedActions, children.length]);
+
+  return (
+    <TabsContext.Provider value={contextValue}>
       <Block name="tabs">
         <Elem name="list">
           {children}
 
-          <Elem
-            tag={Button}
-            name="add"
-            type="text"
-            onClick={onAdd}
-            icon={addIcon}
-          />
+          {allowedActions.add !== false && (
+            <Elem
+              tag={Button}
+              name="add"
+              type="text"
+              onClick={onAdd}
+              icon={addIcon}
+            />
+          )}
         </Elem>
         <Elem name="extra">
           {tabBarExtraContent}
@@ -70,7 +78,7 @@ export const TabsItem = ({
   managable = true,
   virtual = false,
 }) => {
-  const { switchTab, selectedTab, lastTab } = useContext(TabsContext);
+  const { switchTab, selectedTab, lastTab, allowedActions } = useContext(TabsContext);
   const [currentTitle, setCurrentTitle] = useState(title);
   const [renameMode, setRenameMode] = useState(false);
   const [hover, setHover] = useState(false);
@@ -99,7 +107,7 @@ export const TabsItem = ({
       mod={{ active, hover, virtual }}
       onMouseEnter={()=>setHover(true)}
       onMouseLeave={()=>setHover(false)}
-    > 
+    >
       <Elem
         name="item-left"
         onClick={() => switchTab?.(tab)}
@@ -121,7 +129,7 @@ export const TabsItem = ({
             }}
           />
         ) : (
-          <span style={{  
+          <span style={{
             whiteSpace: 'nowrap',
             overflow: 'hidden',
             textOverflow: 'ellipsis',
@@ -138,8 +146,9 @@ export const TabsItem = ({
             align="bottom-left"
             content={(
               <TabsMenu
-                editable={editable}
-                closable={!lastTab && deletable}
+                editable={editable && allowedActions.edit}
+                closable={!lastTab && deletable && allowedActions.delete}
+                clonable={allowedActions.add && allowedActions.duplicate}
                 virtual={virtual}
                 onClick={(action) => {
                   switch(action) {
