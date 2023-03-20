11df6f3cd19d38183d47c13a7946d9a5e2b1887c
Yousif Yassi <yousif@heartex.com>
2023-03-20 16:37:50 -0400

    updating semantic search visuals

--- src/components/Common/SemanticSearch/SemanticSearch.js
+++ src/components/Common/SemanticSearch/SemanticSearch.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
+import { Block, Elem } from "../../../utils/bem";
 import Input from "../Input/Input";
+import { Button } from "../Button/Button";
+import { FaSearch } from "react-icons/fa";
+import { RiFilter3Line } from "react-icons/ri";
+import "./SemanticSearch.styl";
 
 export const SemanticSearch = ({
   value,
   type,
   onChange,
+  onSubmit,
   placeholder,
   schema,
   style,
@@ -13,6 +19,11 @@ export const SemanticSearch = ({
 }) => {
   const inputRef = React.useRef();
   const [currentValue, setCurrentValue] = useState();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submitHandler", currentValue);
+    onSubmit?.(currentValue);
+  };
   const onChangeHandler = () => {
     const value = inputRef.current?.value ?? inputRef.current?.input?.value;
 
@@ -26,15 +37,23 @@ export const SemanticSearch = ({
   }, [value]);
 
   return (
-    <Input
-      size={size ?? "medium"}
-      type={type}
-      value={currentValue ?? ""}
-      ref={inputRef}
-      placeholder={placeholder ?? "Semantic search"}
-      onChange={onChangeHandler}
-      style={style}
-      {...(schema ?? {})}
-    />
+    <Block tag="form" onSubmit={submitHandler} name='semanticSearch'>
+      <Elem name='container'>
+        <Elem tag={FaSearch} name='icon' mod={{ prefix: true }} />
+        <Elem name='input'
+          tag={Input}
+          size={size ?? "medium"}
+          type={type}
+          value={currentValue ?? ""}
+          ref={inputRef}
+          placeholder={placeholder ?? "Semantic search"}
+          onChange={onChangeHandler}
+          style={style}
+          {...(schema ?? {})}
+        />
+        <Elem tag={RiFilter3Line} name='icon' mod={{ filter: true }} />
+      </Elem>
+      <Elem tag={Button} name='button' look="primary">Search</Elem>
+    </Block>
   );
 };
