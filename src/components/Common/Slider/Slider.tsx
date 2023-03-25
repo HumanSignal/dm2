26f4bd6db8d2f187bbbf8bfd2360bf4d0c2b14bc
Yousif Yassi <yousif@heartex.com>
2023-03-25 10:54:00 -0400

    feat: LOPS-72: Create range slider frontend component and add to Semantic Search

---
+++ src/components/Common/Slider/Slider.tsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from "react";
+import { Block, Elem } from "../../../utils/bem";
+import { clamp } from "../../../utils/helpers";
+import "./Resizer.styl";
+
+interface SliderInterface {
+  onFromChange?: (number: number) => void,
+  onToChange?: (number: number) => void,
+  from?: number,
+  to?: number,
+  min?: number,
+  max?: number,
+  step?: number,
+  minDiff?: number,
+};
+
+export const Slider = ({
+  onFromChange,
+  onToChange,
+  min = 0,
+  max = 100,
+  step = clamp(1, min, max),
+  from = clamp(0, min, max),
+  to = clamp(100, min, max),
+  minDiff = 0,
+}: SliderInterface) => {
+  const fromRef = useRef();
+  const toRef = useRef();
+  const [currentFrom, setCurrentFrom] = useState(from);
+  const [currentTo, setCurrentTo] = useState(to);
+  const style = {
+    "--from-pos": `${(from/max) * 100}%`,
+    "--to-pos": `${(to/max) * 100}%`,
+  }
+
+  return (
+    <Block name="slider" style={style}>
+      <Elem name='fill' />
+      <Elem name='handle' ref={fromRef} mod={{left: true}} />
+      <Elem name='handle' ref={toRef} mod={{right: true}} />
+    </Block>
+  );
+};
