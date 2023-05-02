7a3f16e378bc22018968cbe9e5a2c7a4da9444c8
Yousif Yassi <yousif@heartex.com>
2023-05-02 14:18:58 -0400

    clearing out threshold model

--- src/stores/Tabs/semanticsearch_threshold.js
+++
@@ -1,7 +0,0 @@
-import { types } from "mobx-state-tree";
-
-export const ThresholdModel = types
-  .model("ThresholdModel", {
-    from: types.optional(types.number, 0),
-    to: types.optional(types.number, 100),
-  });
