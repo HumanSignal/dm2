002d95496e8eeac73c69386b1abf45169224e146
Travis1282 <travisjosephclark@gmail.com>
2023-09-06 10:23:48 -0500

    fix: lsdv-5044: add queTotal to lafProperties from project

--- src/sdk/lsf-sdk.js
+++ src/sdk/lsf-sdk.js
@@ -170,6 +170,8 @@ export class LSFWrapper {
       forceAutoAnnotation: this.isInteractivePreannotations,
       forceAutoAcceptSuggestions: this.isInteractivePreannotations,
       messages: options.messages,
+      queueTotal: dm.store.project.queue_total,
+
       /* EVENTS */
       onSubmitDraft: this.onSubmitDraft,
       onLabelStudioLoad: this.onLabelStudioLoad,
