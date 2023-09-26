32a18f43f46d65f61db5841ac58258f36f194bdc
bmartel <brandonmartel@gmail.com>
2023-09-26 10:39:21 -0400

    feat: LEAP-206: Ensure users can navigate freely and drafts are saved when leaving the labelling screen (#244)

--- src/components/MainView/DataViewOld/Table.js
+++ src/components/MainView/DataViewOld/Table.js
@@ -4,7 +4,7 @@ import { useCallback, useMemo } from "react";
 import { FaQuestionCircle } from "react-icons/fa";
 import { useShortcut } from "../../../sdk/hotkeys";
 import { Block, Elem } from "../../../utils/bem";
-import { FF_DEV_2536, FF_DEV_4008, isFF } from '../../../utils/feature-flags';
+import { FF_DEV_2536, FF_DEV_4008, FF_OPTIC_2, isFF } from '../../../utils/feature-flags';
 import * as CellViews from "../../CellViews";
 import { Icon } from "../../Common/Icon/Icon";
 import { ImportButton } from "../../Common/SDKButtons";
@@ -121,7 +121,9 @@ export const DataView = injector(
         } else if (e.metaKey || e.ctrlKey) {
           window.open(`./?task=${itemID}`, "_blank");
         } else {
-          await store._sdk.lsf?.saveDraft();
+          if (isFF(FF_OPTIC_2)) {
+            await store._sdk.lsf?.saveDraft();
+          }
           getRoot(view).startLabeling(item);
         }
       },
