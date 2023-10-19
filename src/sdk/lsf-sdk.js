2b141cacebb38ce657db45ea3bc54d988056e720
bmartel <brandonmartel@gmail.com>
2023-10-19 16:07:25 -0500

    Update src/sdk/lsf-sdk.js

--- src/sdk/lsf-sdk.js
+++ src/sdk/lsf-sdk.js
@@ -657,7 +657,6 @@ export class LSFWrapper {
   saveDraft = async (target = null) => {
     const selected = target || this.lsf?.annotationStore?.selected;
     const hasChanges = !!selected?.history.undoIdx && !selected?.submissionStarted;
-    
     if (!hasChanges || !selected) return;
     const res = await selected?.saveDraftImmediatelyWithResults();
     const status = res?.$meta?.status;
