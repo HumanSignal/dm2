e69cbd6caa367e0facdebff50a59daf88dd2e879
Yousif Yassi <yousif@heartex.com>
2023-11-16 13:21:40 -0500

    dont need to send default - if upper limit undefined just dont show the semantic search stuff as its not ready yet

--- src/stores/DataStores/tasks.js
+++ src/stores/DataStores/tasks.js
@@ -8,7 +8,6 @@ import { CustomJSON } from "../types";
 import { FF_DEV_2536, FF_LOPS_E_3, isFF } from "../../utils/feature-flags";
 
 const SIMILARITY_UPPER_LIMIT_PRECISION = 1000;
-const SIMILARITY_UPPER_LIMIT_DEFAULT = 10;
 const fileAttributes = types.model({
   "certainty": types.optional(types.maybeNull(types.number), 0),
   "distance": types.optional(types.maybeNull(types.number), 0),
@@ -227,11 +226,7 @@ export const create = (columns) => {
         if (total_predictions !== null)
           self.totalPredictions = total_predictions;
         if (similarity_score_upper_limit !== null)
-          self.similarityUpperLimit = similarity_score_upper_limit ?(
-            (Math.ceil(similarity_score_upper_limit * SIMILARITY_UPPER_LIMIT_PRECISION) / SIMILARITY_UPPER_LIMIT_PRECISION)
-          ) : (
-            SIMILARITY_UPPER_LIMIT_DEFAULT
-          );
+          self.similarityUpperLimit = (Math.ceil(similarity_score_upper_limit * SIMILARITY_UPPER_LIMIT_PRECISION) / SIMILARITY_UPPER_LIMIT_PRECISION);
       },
 
     }))
